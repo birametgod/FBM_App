089ae794cd993d57845e8f8069e12f30f9c00302
SENE Birame <birametgod95@gmail.com>
2020-02-06 11:09:34 +0100

    begin develop branch

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hello TEAM NOKIA';
+  title = 'Hello TEAM NOKIA (team nokia)';
 }
