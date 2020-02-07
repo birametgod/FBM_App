4a0e5eea760b1ac11261ad13a335ccb4722daccb
DIOP Mamadou <mdiop@Domeo.fr>
2020-02-07 17:32:45 +0100

    creation des components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchProfilComponent } from './search-profil/search-profil.component';
+import { ProfilDeveloperComponent } from './profil-developer/profil-developer.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { FreelanceRegistrationComponent } from './freelance-registration/freelance-registration.component';
+import { CompanyRegistrationComponent } from './company-registration/company-registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchProfilComponent,
+    ProfilDeveloperComponent,
+    AuthentificationComponent,
+    FreelanceRegistrationComponent,
+    CompanyRegistrationComponent
   ],
   imports: [
     BrowserModule
