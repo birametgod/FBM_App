2eddba1fcd4a02dd353c0071b8e0120c64d6e424
DIOP Mamadou <mdiop@Domeo.fr>
2020-02-07 17:32:45 +0100

    creation des components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,11 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatSliderModule } from "@angular/material/slider";
 import { AppComponent } from './app.component';
+import { SearchProfilComponent } from './search-profil/search-profil.component';
+import { ProfilDeveloperComponent } from './profil-developer/profil-developer.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { FreelanceRegistrationComponent } from './freelance-registration/freelance-registration.component';
+import { CompanyRegistrationComponent } from './company-registration/company-registration.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchProfilComponent,
+    ProfilDeveloperComponent,
+    AuthentificationComponent,
+    FreelanceRegistrationComponent,
+    CompanyRegistrationComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/profil-developer/profil-developer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profil-developer',
+  templateUrl: './profil-developer.component.html',
+  styleUrls: ['./profil-developer.component.css']
+})
+export class ProfilDeveloperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
