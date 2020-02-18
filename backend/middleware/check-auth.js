026f77beb066cfdb79a006b99d65267c78f075b6
SENE Birame <birametgod95@gmail.com>
2020-02-18 22:16:39 +0100

    manage authorization

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -1,15 +1,31 @@
-import  * as jwt from "jsonwebtoken";
+import * as jwt from "jsonwebtoken";
 
-module.exports = (req, res, next) => {
-  try {
-    const token = req.headers.authorization.split(' ')[1];
-    const userData = jwt.verify(token, 'my_token_secret');
-    req.userData = userData;
-    next();
-  } catch (err) {
-    res.status(401).json({
-      message: 'you are not authenticated',
-      err: err
-    });
+export function checkAuth(roles = []) {
+  // roles param can be a single role string ('User') 
+  // or an array of roles (['Admin', 'User'])
+  if (typeof roles === 'string') {
+    roles = [roles];
   }
-};
+
+  return [
+    (req, res, next) => {
+      try {
+        const token = req.headers.authorization.split(' ')[1];
+        const userData = jwt.verify(token, 'my_token_secret');
+        // authorize based on user role
+        if (roles.length && !roles.includes(userData.role)) {
+          // user's role is not authorized
+          return res.status(401).json({
+            message: 'Unauthorized'
+          });
+        }
+      } catch (error) {
+        return res.status(401).json({
+          message: 'Unauthorized',
+        });
+      }
+      // authentication and authorization successful
+      next();
+    }
+  ];
+}
